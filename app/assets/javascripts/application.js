0c91344fd6eed925fae138754d05cde601ee1952
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-12-05 03:30:30 -0500

    nav bar is now solid black when you're at the top of the page, but goes sheer when you scroll down

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -106,5 +106,14 @@ function createMarker(position) {
  });
 }
 
-
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 60) {
+            $("nav").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $("nav").removeClass("active");
+        }
+    });
+});
 
