ad2fc6b8367df889e7a0b5c7d1a61be6a922f29f
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-12-05 03:03:15 -0500

    human and vampire  cards now work -alternate text for each- can choose skintypes - map shows current location full width

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,94 @@
 //= require materialize-sprockets
 //= require materialize/extras/nouislider
 
+console.log("APP ONLINE");
+
+var map;
+
 $(document).ready(function(){
+
+	if ("geolocation" in navigator){
+	  navigator.geolocation.getCurrentPosition(onLocation, onError);
+	}	
+
+
  	$('.parallax').parallax();
-    $(".button-collapse").sideNav();
-    $('.slider').slider({full_width: true});
+  $(".button-collapse").sideNav();
+  $('.slider').slider({full_width: true});
+  $('select').material_select();
+
+  $("#human").on("click", intoHuman);
+	$("#vampire").on("click", intoVampire);
+
 
 });
+
+
+function intoHuman (event) {
+	event.preventDefault();
+	console.log("User is a human");
+	// should this toggle content between human and vampire?
+	$(".hello-vampire").toggle();
+	$(".hello-human").toggle();
+
+	$(".vampire-form").toggle();
+	$(".human-form").toggle();
+
+}
+
+function intoVampire (event) {
+	event.preventDefault();
+	console.log("User is a vampire");
+	// should this toggle content between human and vampire?
+	$(".hello-vampire").toggle();
+	$(".hello-human").toggle();
+
+	$(".vampire-form").toggle();
+	$(".human-form").toggle();
+
+	$(".adjective").text("indecisive");
+
+}
+
+function onLocation(position){
+
+  console.log("Getting location");
+
+  var myPosition = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+
+  createMap(myPosition);
+}
+
+function onError(err){
+  console.log("Error locating your position. Please update your browser.", err);
+}
+
+function createMap(position){
+  var mapOptions = {
+    center: position,
+    zoom: 12
+  };
+  map = new google.maps.Map($('#map-canvas')[0], mapOptions);
+  createMarker(position);
+  createMarker({lat: 25.8068102, lng: -80.201181}); 
+}
+
+function createMarker(position) {
+  var marker = new google.maps.Marker({
+   position: position,
+   map: map
+ });
+}
+
+function createMarker(position) {
+  var marker = new google.maps.Marker({
+   position: position,
+   map: map
+ });
+}
+
+
 
